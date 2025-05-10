d615b507ebbfd96d1f38d5ad083c996825376007
= <=>
2025-05-10 17:48:58 +0530

    Backend initial commit

---
+++ app-backend/src/index.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response, type Express } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import Deploy from "./routes/deploy"
+
+interface DeployRequestBody {
+   projectPath: string;
+   appName: string;
+}
+
+const app: Express = express();
+const PORT = process.env.PORT ?? 8000;
+app.use(bodyParser.json());
+// app.use(
+//    cors({
+//       origin: function (origin, callback) {
+//          const allowedOrigins = [
+//             "http://localhost:3000",
+//          ];
+//          if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true);
+//          } else {
+//             callback(new Error("Not allowed by CORS"));
+//          }
+//       },
+//       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//       allowedHeaders: [
+//          "Content-Type",
+//          "Authorization",
+//          "X-Requested-With",
+//          "Accept",
+//       ],
+//       credentials: true,
+//    })
+// );
+// app.options("*", cors());
+
+// Middleware to parse JSON bodies
+// app.use(express.static("public"));
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (req, res) => {
+   res.send("Hello World!");
+});
+
+app.use("/deploy", Deploy)
+
+app.listen(PORT, () => console.log(`🚀 Server is running at http://localhost:${PORT}`));
+
+module.exports = app;
---
+++ app-backend/src/routes/deploy.ts
@@ -0,0 +1,44 @@
+import { execFile } from "child_process";
+import { Router } from "express";
+import path from "path";
+
+const router = Router();
+
+router.post("/", async (req, res, next) => {
+   try {
+      const projectPath = process.env.PROJECT_PATH;
+      const appName = process.env.APP_NAME;
+
+      if (!projectPath || !appName) {
+         return res
+            .status(400)
+            .json({ error: "Missing projectPath or appName" });
+      }
+
+      const scriptPath = path.join(__dirname, "../../", "deploy.sh");
+
+      const envVars = {
+         ...process.env,
+         PROJECT_PATH: projectPath,
+         APP_NAME: appName,
+      };
+
+      execFile(scriptPath, { env: envVars }, (error, stdout, stderr) => {
+         if (error) {
+            console.error("❌ Deployment failed:", stderr);
+            return res.status(500).json({ error: stderr });
+         }
+
+         console.log("✅ Deployment output:\n", stdout);
+         return res.status(200).json({
+            message: "Deployment successful",
+            output: stdout,
+         });
+      });
+   } catch (error) {
+    console.log(error)
+    next(error)
+   }
+});
+
+export default router;

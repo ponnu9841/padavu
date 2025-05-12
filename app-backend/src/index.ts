772805b9ac1b7f381f80c4f67d9c9b426368b95c
= <=>
2025-05-12 09:01:32 +0530

    Hides cors for now

--- app-backend/src/index.ts
+++ app-backend/src/index.ts
@@ -8,29 +8,29 @@ dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT ?? 8000;
 app.use(bodyParser.json());
-app.use(
-   cors({
-      origin: function (origin, callback) {
-         const allowedOrigins = [
-            "http://localhost:3000",
-         ];
-         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-         } else {
-            callback(new Error("Not allowed by CORS"));
-         }
-      },
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: [
-         "Content-Type",
-         "Authorization",
-         "X-Requested-With",
-         "Accept",
-      ],
-      credentials: true,
-   })
-);
-app.options("*", cors());
+// app.use(
+//    cors({
+//       origin: function (origin, callback) {
+//          const allowedOrigins = [
+//             "http://localhost:3000",
+//          ];
+//          if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true);
+//          } else {
+//             callback(new Error("Not allowed by CORS"));
+//          }
+//       },
+//       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//       allowedHeaders: [
+//          "Content-Type",
+//          "Authorization",
+//          "X-Requested-With",
+//          "Accept",
+//       ],
+//       credentials: true,
+//    })
+// );
+// app.options("*", cors());
 
 // Middleware to parse JSON bodies
 app.use(express.static("public"));

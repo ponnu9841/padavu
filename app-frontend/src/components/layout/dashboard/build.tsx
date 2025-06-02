cfc9382512ff4875a0c6a5548cd6f79448c45adf
= <=>
2025-06-02 08:45:46 +0530

    Build app to stream

--- app-frontend/src/components/layout/dashboard/build.tsx
+++ app-frontend/src/components/layout/dashboard/build.tsx
@@ -7,15 +7,41 @@ import {
    DialogTitle,
    DialogTrigger,
 } from "@/components/ui/dialog";
-import axiosInstance from "@/lib/axios";
+import { getToken } from "@/lib/local-storage-service";
 import { useState } from "react";
 
 export default function BuildApplication() {
    const [loading, setLoading] = useState(false);
+   const [data, setData] = useState("");
+
    const handleBuild = async () => {
       setLoading(true);
       try {
-         const response = await axiosInstance.get("/deploy");
+
+         const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/api/deploy`,
+            {
+               method: "GET",
+               headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${getToken()}`,
+               },
+            }
+         );
+
+         const decoder = new TextDecoder("utf-8");
+         const reader = response.body?.getReader();
+         let accumulatedData = "";
+
+         if (!reader) return;
+
+         while (true) {
+            const { value, done } = await reader.read();
+            if (done) break;
+            accumulatedData += decoder.decode(value, { stream: true });
+            setData(accumulatedData);
+         }
+
          if (response) {
             window.location.reload();
          }
@@ -25,6 +51,7 @@ export default function BuildApplication() {
          setLoading(false);
       }
    };
+
    return (
       <Dialog>
          <DialogTrigger asChild>
@@ -34,8 +61,11 @@ export default function BuildApplication() {
             <DialogHeader>
                <DialogTitle>Build Application</DialogTitle>
                <DialogDescription>Build Your Application</DialogDescription>
+               <p className="py-3">{data}</p>
             </DialogHeader>
-            <Button onClick={handleBuild} disabled={loading}>{loading ? "Building..." : "Build"}</Button>
+            <Button onClick={handleBuild} disabled={loading}>
+               {loading ? "Building..." : "Build"}
+            </Button>
          </DialogContent>
       </Dialog>
    );

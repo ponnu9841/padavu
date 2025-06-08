bdd8af2554055d1e284e1f277ea18a949940d281
= <=>
2025-06-08 07:27:48 +0530

    Fixes build

--- app-frontend/src/components/sections/blog/index.tsx
+++ app-frontend/src/components/sections/blog/index.tsx
@@ -1,35 +1,63 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import BlogCard from "./blog-card";
 // import TitleBadge from "@/components/custom/title-badge";
 // import Heading from "@/components/custom/heading";
-import SectionTitle from "@/components/custom/section-title";
+import { useAppDispatch, useAppSelector } from "@/hooks/use-store";
+import { fetchBlogs, setPageNo } from "@/store/features/blogs-slice";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Pagination } from "@/components/pagination";
 
-interface BlogPropsType {
-	blogs: BlogProps[];
-}
+export default function Blog() {
+   const dispatch = useAppDispatch();
+   useEffect(() => {
+      const controller = new AbortController();
+      dispatch(fetchBlogs({ controller }));
+      return () => controller.abort();
+   }, []); //eslint-disable-line
 
-export default function Blog(props: BlogPropsType) {
-	return (
-		<>
-			<SectionTitle
-				title="We are a full-service creative agency"
-				description="Our team of designers, developers and creatives are perfectionists who love what they do and love"
-				headingAnimation="fadeInDown"
-			/>
-			{/* <div className="flex justify-center mb-4">
-				<TitleBadge title="Blog" />
-			</div> 
-			<Heading title="Latest Blog Posts" className="text-center mb-12" />*/}
-			<div className="flex flex-col md:flex-row gap-8 flex-wrap items-stretch justify-center mt-12">
-				{props.blogs.map((blog, index) => (
-					<div
-						className="md:w-[calc(50%-1rem)] lg:w-[calc(32%-1rem)]"
-						key={index}
-					>
-						<BlogCard key={index} {...blog} />
-					</div>
-				))}
-			</div>
-		</>
-	);
+   const { blogs, loading, pageNo } = useAppSelector((state) => state.blogs);
+   const lastPage = blogs?.totalPages;
+   return (
+      <>
+         {!loading && blogs?.data.length === 0 && (
+            <div className="col-span-4 text-center text-red-500 mt-12 md:mt-20">
+               No Record Found
+            </div>
+         )}
+
+         {loading &&
+            Array(6)
+               .fill(null)
+               .map((_, index) => (
+                  <Skeleton key={index} className="aspect-square" />
+               ))}
+
+         <div className="container">
+            {!!blogs?.data.length && (
+               <div className="flex flex-col md:flex-row gap-8 flex-wrap items-stretch justify-center mt-12">
+                  {blogs?.data.map((blog) => (
+                     <div
+                        className="md:w-[calc(50%-1rem)] lg:w-[calc(32%-1rem)] min-h-[400px]"
+                        key={blog.id}
+                     >
+                        <BlogCard {...blog} />
+                     </div>
+                  ))}
+               </div>
+            )}
+            {!loading && blogs?.data.length ? (
+               <div className="mt-6">
+                  <Pagination
+                     pageNo={pageNo}
+                     setPageNo={(pageNo) => dispatch(setPageNo(pageNo))}
+                     totalPages={lastPage || 1}
+                  />
+               </div>
+            ) : (
+               <></>
+            )}
+         </div>
+      </>
+   );
 }
